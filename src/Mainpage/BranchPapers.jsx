15e9136d021083459b24d56b106ea18af28bbc5f
jaganreddy22 <bjaganmohanreddy22@gmail.com>
2026-02-20 22:36:06 +0530

    addded

--- src/Mainpage/BranchPapers.jsx
+++ src/Mainpage/BranchPapers.jsx
@@ -4,37 +4,90 @@ import './BranchPage.css';
 
 const papers = {
   csm: [
-    { title: '2-1 Mid 1 Papers', file: '/files/csmmid1.pdf' },
-    { title: '2-1 Mid 2 Papers', file: '/files/csmmid1.pdf' },
-    { title: '2-1 SEM Papers', file: '/files/csmsem.pdf' },
-    { title: '2-2 Mid 1 Papers', file: '/files/csm2-2mid1.pdf' },
-     { title: '2-2 Mid 2 Papers', file: '/files/csm2-2mid2.pdf' },
-     {title:'2-2  sem Papers',file:'/files/csm2-2sem.pdf'}
-    
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' }
+,    { title: '2-1 Mid 1 Papers 2024', file: window.location.origin + '/questionpaper/csmmid1.pdf' },
+    { title: '2-1 Mid 2 Papers 2024', file: window.location.origin + '/questionpaper/csmmid2.pdf' },
+    { title: '2-1 SEM Papers 2024 Nov/Dec(2024)', file: window.location.origin + '/questionpaper/csmsem.pdf' },
+    { title: '2-2 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/csm2-2mid1.pdf' },
+    { title: '2-2 Mid 2 Papers 2025', file: window.location.origin + '/questionpaper/csm2-2mid2.pdf' },
+    { title: '2-2 Sem Papers May/June(2025)', file: window.location.origin + '/questionpaper/csm2-2sem.pdf' },
+    { title: '2-2 mid 1 (2026)', file: window.location.origin + '/questionpaper/2026mid1.pdf' },
+    { title: '3-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/3-1mid1.pdf' },
+    { title: '3-1 Mid 2 Papers 2025', file: window.location.origin + '/questionpaper/3-1mid2.pdf' },
+    { title: '3-1 Sem Papers Nov/Dec(2025)', file: window.location.origin + '/questionpaper/3-1sem.pdf' },
+    { title: '3-2 Mid 1 Papers', file: window.location.origin + '/questionpaper/3-2mid1.pdf' },
   ],
+
   cai: [
-    { title: '2-1 Mid 1 Papers', file: '/files/csmmid1.pdf' },
-    { title: '2-1 Mid 2 Papers', file: '/files/csmmid2.pdf' },
-    { title: '2-1 SEM Papers', file: '/files/csmsem.pdf' },
-    { title: '2-2 Mid 1 Papers', file: '/files/csm2-2mid1.pdf' },
-    { title: '2-2 Mid 2 Papers', file: '/files/csm2-2mid2.pdf' },
-      {title:'2-2  sem Papers',file:'/files/csm2-2sem.pdf'}
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+    { title: '2-1 Mid 1 Papers 2024', file: window.location.origin + '/questionpaper/csmmid1.pdf' },
+    { title: '2-1 Mid 2 Papers 2024', file: window.location.origin + '/questionpaper/csmmid2.pdf' },
+    { title: '2-1 SEM Papers 2024 Nov/Dec(2024)', file: window.location.origin + '/questionpaper/csmsem.pdf' },
+    { title: '2-2 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/csm2-2mid1.pdf' },
+    { title: '2-2 Mid 2 Papers 2025', file: window.location.origin + '/questionpaper/csm2-2mid2.pdf' },
+    { title: '2-2 Sem Papers May/June(2025)', file: window.location.origin + '/questionpaper/csm2-2sem.pdf' },
+    { title: '2-2 mid 1 (2026)', file: window.location.origin + '/questionpaper/2026mid1.pdf' },
+    { title: '3-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/3-1mid1.pdf' },
+    { title: '3-1 Mid 2 Papers 2025', file: window.location.origin + '/questionpaper/3-1mid2.pdf' },
+    { title: '3-1 Sem Papers Nov/Dec(2025)', file: window.location.origin + '/questionpaper/3-1sem.pdf' }
   ],
+
   cse: [
-    { title: '2-1 Mid 1 Papers', file: '/files/mid1.pdf' },
-    { title: '2-1 Mid 2 Papers', file: '/files/mid2.pdf' },
-    { title: '2-1 SEM Papers', file: '/files/csmsem.pdf' },
-    { title: '2-2 Mid 1 Papers', file: '/files/csm2-2mid1.pdf' },
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+    { title: '2-1 Mid 1 Papers 2024', file: window.location.origin + '/questionpaper/csmmid1.pdf' },
+    { title: '2-1 Mid 2 Papers 2024', file: window.location.origin + '/questionpaper/csmmid2.pdf' },
+    { title: '2-1 SEM Papers 2024 Nov/Dec(2024)', file: window.location.origin + '/questionpaper/csmsem.pdf' },
+    { title: '2-2 Mid 1 Papers 2025 ', file: window.location.origin + '/questionpaper/csm2-2mid1.pdf' }
   ],
+  csd:[
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+
+  ],
+  csc:[
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+  ],
+  csbs:[
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+  ],
+  it:[
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+  ],
+  ece:[
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+  ],
+  eee:
+  [
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+  ],
+  civil:[
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+  ],
+  mech:
+  [
+    { title: '1-1 Mid 1 Papers 2025', file: window.location.origin + '/questionpaper/1styearmid-12026.pdf' },
+    { title: '1-1 Sem Papers Dec(2025)/Jan(2026)', file: window.location.origin + '/questionpaper/1styearsem2026.pdf' },
+  ],
+    
 };
 
 const BranchPapers = () => {
   const { branchCode } = useParams();
-  const branchName = branchCode.toUpperCase();
+  const branchName = branchCode?.toUpperCase();
   const branchPapers = papers[branchCode] || [];
 
   return (
     <div>
+
       {/* Navbar */}
       <header className="app-header">
         <ul className="nav-bar">
@@ -54,7 +107,17 @@ const BranchPapers = () => {
           {branchPapers.map((item, idx) => (
             <div className="resource-card" key={idx}>
               <h3>{item.title}</h3>
-              <a className="download-btn" href={item.file} download>Download</a>
+
+              <a
+                className="download-btn"
+                href={item.file}
+                download={item.title + ".pdf"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Download
+              </a>
+
             </div>
           ))}
           {branchPapers.length === 0 && <p>No question papers available for this branch.</p>}
